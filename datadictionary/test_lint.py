15afbf4676e29b49e29e6758a6c85ce0bca8bb16
Erich Schulz <erichbschulz@gmail.com>
2020-08-02 18:42:39 +1000

    patch code include duplicate keys

--- datadictionary/test_lint.py
+++ datadictionary/test_lint.py
@@ -2,11 +2,43 @@
 import yaml, os, re, glob
 import pytest
 dictionary_path = 'datadictionary'
-verbose = True or False
+
 log = print
-def noop(x):
-    pass
-log = print if verbose else noop
+from yaml import Loader
+
+from yaml.constructor import ConstructorError
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+
+class UniqueKeyLoader(Loader):
+    def construct_mapping(self, node, deep=False):
+        if not isinstance(node, yaml.nodes.MappingNode):
+            raise ConstructorError(None, None,
+                    "expected a mapping node, but found %s" % node.id,
+                    node.start_mark)
+        mapping = {}
+        for key_node, value_node in node.value:
+            key = self.construct_object(key_node, deep=deep)
+            try:
+                hash(key)
+            except TypeError:
+                raise ConstructorError(
+                       f"while constructing a mapping", node.start_mark,
+                       f"found unacceptable key ({key}) {key_node.start_mark}")
+            # check for duplicate keys
+            if key in mapping:
+                raise ConstructorError(
+                  f"duplicate key [{key}] line: {key_node.start_mark.line}")
+            value = self.construct_object(value_node, deep=deep)
+            mapping[key] = value
+        return mapping
+
+def test_frish():
+    assert True
 
 # compare the fields i an item to a list to see if we have
 # interchangeable fields
@@ -25,6 +57,9 @@ meta_meta = {
         'optional': {
             'units': {},
             'enum': {},
+            'min': {},
+            'max': {},
+            'default': {},
             },
         'extra_checks': (lambda i: must_have_n_of(i, ['units', 'enum'])),
         'key_regex': '^[A-Z]+([A-Z0-9])*(_[A-Z0-9]+)*$',
@@ -77,7 +112,7 @@ def test_lint():
         allowed = {**required, **meta['optional']}
         key_pattern = re.compile(meta['key_regex'])
         yaml_text = open(os.path.join(dictionary_path, f'{f}.yaml'), 'r').read()
-        data[f] = yaml.safe_load(yaml_text)
+        data[f] = yaml.load(yaml_text, Loader=UniqueKeyLoader)
         for key, fields in data[f].items(): # loop over items
             extra = fields.keys() - allowed.keys()
             missing = required.keys() - fields.keys()
