47e7e909f4ec8c37e6feb2cb4fc97f043429cca7
Erich Schulz <erichbschulz@gmail.com>
2020-08-03 16:39:07 +1000

    tweak the watch script

--- devbin/watch.sh
+++ devbin/watch.sh
@@ -2,9 +2,11 @@
 echo "starting will run '$2' on any change to '$1'"
 inotifywait -mr -e close_write $1 |
 while read D E F; do
-  echo "----------------hello $D $E $F"
+  # echo "----------------hello $D $E $F"
   if [[ "$F" == *.py || "$F" == *.yaml ]]
   then
-    $2
+    echo "starting"
+    eval "$2"
+    echo "done"
   fi
 done
