2ee0264a384d9cee8871308dc9faca6ff2e148ed
Erich Schulz <erichbschulz@gmail.com>
2020-08-06 14:25:06 +1000

    autopep

--- datadictionary/test_lint.py
+++ datadictionary/test_lint.py
@@ -1,27 +1,33 @@
-import yaml, os, re, glob
+import yaml
+import os
+import re
+import glob
 import pytest
 import ventos_yaml as vy
 log = print
 
+
 def test_all_files_have_meta():
-    base = lambda f: os.path.splitext(os.path.basename(f))[0]
+    def base(f): return os.path.splitext(os.path.basename(f))[0]
     dictionary_yaml_glob = glob.glob(vy.yaml_file(vy.DICTIONARY_PATH, '*'))
     files = [base(f) for f in dictionary_yaml_glob]
     assert set(files) == set(vy.META_ITEMS.keys())
+
 
 def test_lint():
     data = vy.load_yaml()
     # generic linting
     error = {}
+
     def log_error(key_list, err):
         key = '-'.join(key_list)
         error[key] = error.get(key, [])
         error[key].append(err)
-    for f, meta in vy.META_ITEMS.items(): # loop over files
+    for f, meta in vy.META_ITEMS.items():  # loop over files
         required = meta['required']
         allowed = {**required, **meta['optional']}
         key_pattern = re.compile(meta['key_regex'])
-        for key, fields in data[f].items(): # loop over items
+        for key, fields in data[f].items():  # loop over items
             extra = fields.keys() - allowed.keys()
             missing = required.keys() - fields.keys()
             if len(missing) + len(extra):
@@ -31,17 +37,17 @@ def test_lint():
                     log_error([f, key], f'extra: {extra}')
             if not key_pattern.match(key):
                 log_error([f, key],
-                    f"#### key: {key} does not match {meta['key_regex']}")
+                          f"#### key: {key} does not match {meta['key_regex']}")
             for field, val in fields.items():
                 field_meta = allowed.get(field, {})
                 enum = field_meta.get('enum', False)
                 if enum and val not in enum:
                     log_error([f, key, field],
-                    f'#### unknown value: "{val}" expecting one of {enum}')
+                              f'#### unknown value: "{val}" expecting one of {enum}')
                 field_type = field_meta.get('type', False)
                 if field_type and type(val) not in field_type:
                     log_error([f, key, field],
-                    f'#### bad type: "{val}" got {type(val)} expecting {field_type}')
+                              f'#### bad type: "{val}" got {type(val)} expecting {field_type}')
             for extra_check in meta.get('extra_checks', []):
                 extra_error = extra_check(key, fields, meta, data)
                 if extra_error:
@@ -49,6 +55,8 @@ def test_lint():
     assert error == {}
 
 # Note this test does not include writing the files or verifying file timestamps or size etc
+
+
 def test_package():
     import package
 
